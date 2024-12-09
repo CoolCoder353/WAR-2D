f1d9719d1ac3ac00cbc881a6e404d0033c090048
CoolCoder64 <github@otmfamily.com>
2024-12-09 12:57:32 +1000

    Update WorldStateManager.cs

--- Assets/Scripts/Unit UnNetworked/PathFinding/WorldStateManager.cs
+++ Assets/Scripts/Unit UnNetworked/PathFinding/WorldStateManager.cs
@@ -1,7 +1,6 @@
 using System;
 using Unity.Collections;
-using Unity.Collections.LowLevel.Unsafe;
-using Unity.Entities.UniversalDelegates;
+using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Tilemaps;
