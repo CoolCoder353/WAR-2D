245b0fec4d497daa16c6dcb54018131701c4eaa5
CoolCoder64 <github@otmfamily.com>
2024-12-18 14:00:16 +1000

    ReOrganised Files

--- Assets/Scripts/Unit/Pathfinding/Pathfinding.cs
+++ Assets/Scripts/Unit/Pathfinding/Pathfinding.cs
@@ -122,7 +122,7 @@ public static class Pathfinding
         return;
     }
 
-    public static Path FindPath(TilemapStruct tilemap, int2 start, int2 end, HCostMethod hCostMethod = HCostMethod.Euclidean, int maxIterations = 1000, PathfindingVisualizer visualizer = null)
+    public static Path FindPath(TilemapStruct tilemap, int2 start, int2 end, HCostMethod hCostMethod = HCostMethod.Euclidean, int maxIterations = 1000)
     {
         if (start.Equals(end))
         {
@@ -180,10 +180,6 @@ public static class Pathfinding
                 connections[newPathNode.position] = currentNode;
                 openSet.Enqueue(newPathNode);
             }
-            if (visualizer != null)
-            {
-                UseVisualizer(visualizer, openSet, closedSet);
-            }
         }
         return new Path(new PathNode[0], 0, 0);
     }
@@ -216,127 +212,6 @@ public static class Pathfinding
                 throw new ArgumentOutOfRangeException();
         }
     }
-
-    // Calculate path using D* Lite
-    [Obsolete("This method is not working correctly, use FindPath or BurstFindPath instead")]
-    public static Path FindPathDLite(TilemapStruct tilemap, int2 start, int2 end, HCostMethod hCostMethod = HCostMethod.Euclidean, int maxIterations = 100000, PathfindingVisualizer visualizer = null)
-    {
-        if (start.Equals(end))
-        {
-            return new Path(new PathNode[0], 0, 0);
-        }
-
-        var openSet = new PriorityQueue<PathNode>(tilemap.width * tilemap.height, (x, y) => (x.fcost).CompareTo(y.fcost));
-        var closedSet = new HashSet<int2>();
-        var gScore = new Dictionary<int2, float>();
-        var rhs = new Dictionary<int2, float>();
-        var cameFrom = new Dictionary<int2, int2>();
-
-        var validNeighbours = new NativeHashMap<int2, PathNode>();
-        var invalidNeighbours = new HashSet<int2>();
-
-        gScore[start] = float.MaxValue;
-        rhs[start] = 0;
-        gScore[end] = float.MaxValue;
-        rhs[end] = float.MaxValue;
-
-        openSet.Enqueue(new PathNode { position = start, hcost = CalculateHCost(start, end, hCostMethod) });
-
-        int iterations = 0;
-
-        while (openSet.Count > 0 && iterations < maxIterations)
-        {
-            iterations++;
-
-            var current = openSet.Dequeue();
-
-            if (closedSet.Contains(current.position))
-            {
-                continue;
-            }
-
-            if (current.position.Equals(end) && gScore[current.position] == rhs[current.position])
-            {
-                break;
-            }
-
-            closedSet.Add(current.position);
-
-            foreach (PathNode neighbor in GetNeighbours(tilemap, current, validNeighbours, invalidNeighbours))
-            {
-                if (closedSet.Contains(neighbor.position))
-                {
-                    continue;
-                }
-
-                float tentativeGScore = gScore[current.position] + CalculateHCost(current.position, neighbor.position, hCostMethod);
-                if (!gScore.ContainsKey(neighbor.position) || tentativeGScore < gScore[neighbor.position])
-                {
-                    gScore[neighbor.position] = tentativeGScore;
-
-                    if (!rhs.TryGetValue(neighbor.position, out float neighborRhs))
-                    {
-                        neighborRhs = float.MaxValue;
-                    }
-                    rhs[neighbor.position] = Math.Min(neighborRhs, tentativeGScore);
-                    openSet.Enqueue(new PathNode { position = neighbor.position, hcost = gScore[neighbor.position] + CalculateHCost(current.position, neighbor.position, hCostMethod) });
-
-                    cameFrom[neighbor.position] = current.position;
-                }
-            }
-            if (visualizer != null)
-            {
-                UseVisualizer(visualizer, openSet, closedSet);
-            }
-        }
-
-        if (iterations >= maxIterations)
-        {
-            Debug.LogWarning("Max iterations reached");
-            return new Path(new PathNode[0], 0, 0);
-        }
-
-        if (gScore[end] == float.MaxValue)
-        {
-            return new Path(new PathNode[0], 0, 0);
-        }
-
-        Debug.Log($"Found path with a cost of {gScore[end]} after {iterations} iterations");
-        var path = new List<PathNode>();
-        int2 currentpos = end;
-        while (!currentpos.Equals(start))
-        {
-            path.Add(new PathNode { position = currentpos });
-            currentpos = cameFrom[currentpos];
-        }
-        path.Add(new PathNode { position = start });
-        path.Reverse();
-
-        return new Path(path.ToArray(), path.Count, gScore[end]);
-    }
-
-    private static void UseVisualizer(PathfindingVisualizer visualizer, PriorityQueue<PathNode> openSet, HashSet<int2> closedSet)
-    {
-        if (visualizer != null)
-        {
-            visualizer.SetOpenMap(openSet);
-            visualizer.SetClosedMap(closedSet);
-            // Delay the loop to make it easier to see the pathfinding, Do not use on the main thread 
-            System.Threading.Thread.Sleep(visualizer.delay);
-        }
-    }
-
-    private static void BurstUseVisualizer(ref PathfindingVisualizer visualizer, NativePriorityQueue openSet, NativeHashSet<int2> closedSet)
-    {
-        if (visualizer != null)
-        {
-            visualizer.BurstSetOpenMap(openSet);
-            visualizer.BurstSetClosedMap(closedSet);
-            // Delay the loop to make it easier to see the pathfinding, Do not use on the main thread 
-            System.Threading.Thread.Sleep(visualizer.delay);
-        }
-    }
-
     [BurstCompile]
     private static void BurstRetracePath(ref PathNode endNode, ref NativeHashMap<int2, PathNode> connections, out BurstPath path, Allocator allocator = Allocator.Temp)
     {
